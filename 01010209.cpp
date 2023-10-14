a23450114d08b5d8fa6c2c9984343e0395370a41
Dlmitriyyy <streltsowdm@gmail.com>
2023-10-14 16:21:18 +0300

    Добавьте файлы проекта.

---
+++ 01010209.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	system("chcp 1251 > nul");
+	int x;
+	int result;
+	cout << "Введіть число Х \n";
+	cin >> x;
+	result = 3 * (x * x * x) - 2 * (x * x) + 3 * x - 1;
+	cout << "Отримане число: " << result;
+	return 0;
+}
